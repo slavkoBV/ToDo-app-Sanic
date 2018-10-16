6bc014445fe11bb8c6988aef5cbf378e9bae24f2
slavkoBV <slav_b@ukr.net>
2018-10-16 10:37:23 +0300

    add two more tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-import asynctest
+from asynctest import TestCase
 from app import app
+
 
 tasks = [
     {
@@ -17,9 +18,17 @@ tasks = [
 ]
 
 
-class TestTodoApp(asynctest.TestCase):
+class TestTodoApp(TestCase):
+
     def test_get_tasks(self):
         response = app.test_client.get('/todo/tasks', gather_request=False)
         self.assertEqual(response.status, 200)
         self.assertTrue('tasks' in response.body.decode())
-        self.assertEqual(len(response.json['tasks']), 2)
+
+    def test_get_task_by_id(self):
+        response = app.test_client.get('/todo/tasks/1', gather_request=False)
+        self.assertEqual(response.status, 200)
+
+    def test_get_404_task_by_incorrect_id(self):
+        response = app.test_client.get('/todo/tasks/3454654434asasasa', gather_request=False)
+        self.assertEqual(response.status, 404)
